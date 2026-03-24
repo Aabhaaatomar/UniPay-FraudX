48b15499890ca9bf5bb637d1b712666499aaa9dd
SakshiRajawat562 <rajawatsakshi562@gmail.com>
2026-03-24 22:52:01 +0530

    Update transactions.py

--- transactions.py
+++ transactions.py
@@ -8,3 +8,15 @@ from app.models import Transaction, Blocklist
 from app.services.fraud_detector import analyze_transaction
 from app.services.alert_service import create_alert
 
+transactions_bp = Blueprint("transactions", __name__)
+
+
+def _get_blocklist_values():
+    return {b.value for b in Blocklist.query.all()}
+
+
+def _get_known_devices(user_id):
+    txns = Transaction.query.filter_by(user_id=user_id).all()
+    return {t.device_id for t in txns if t.device_id}
+
+

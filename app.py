31df8a56e155a8aa309d6353f790bbe7ea73bc10
Aabha Tomar <tomaraabha2@gmail.com>
2026-03-24 16:45:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Load Dataset (Excel file)
-df = pd.read_excel(
-    "unipay_fraudx_dataset.xlsx",
-    engine="openpyxl"
-)
+# For local file (replace 'your_dataset.xlsx' with actual path)
+df = pd.read_excel('data.xlsx', engine='openpyxl')
+
 
 @app.route("/")
 def home():
@@ -16,7 +14,25 @@ def home():
 @app.route("/dashboard")
 def dashboard():
     transactions = df.head(15).to_dict(orient="records")
-    return render_template("dashboard.html", transactions=transactions)
+    total_txn = len(df)
+    suspicious_txn = len(df[df["label"] == "Suspicious"])
+    normal_txn = len(df[df["label"] == "Normal"])
+
+    # Hour-wise transactions
+    hour_data = df["hour"].value_counts().sort_index()
+
+    hours = list(map(int, hour_data.index))
+    counts = list(map(int, hour_data.values))
+
+    return render_template(
+        "dashboard.html",
+        transactions=transactions,
+        total_txn=total_txn,
+        suspicious_txn=suspicious_txn,
+        normal_txn=normal_txn,
+        hours=hours,
+        counts=counts
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
